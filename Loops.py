0e8a2bd783116e9e5bb120e4e15165bb468ca264
peak_1604 <65761175+peakeiei@users.noreply.github.com>
2022-11-13 20:48:25 +0700

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print (i**2)
+    #https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen 
